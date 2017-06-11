f6d6ac14e13b8f6b123d34259a7ce5966d5c61ff
Andrin <andrin.bertschi@gmail.com>
2017-06-11 15:33:43 +0200

    wip

--- app/src/main/java/ch/abertschi/adfree/plugin/localmusic/LocalMusicView.kt
+++ app/src/main/java/ch/abertschi/adfree/plugin/localmusic/LocalMusicView.kt
@@ -6,9 +6,30 @@
 
 package ch.abertschi.adfree.plugin.localmusic
 
+import android.content.Intent
+import android.support.v4.app.ActivityCompat
+import android.widget.Toast
+
 /**
  * Created by abertschi on 01.05.17.
  */
 class LocalMusicView {
+
+    private val FILE_SELECT_CODE: Int = 1
+
+//    fun chooseFile() {
+//        val intent = Intent(Intent.ACTION_GET_CONTENT)
+//        intent.setType("*/*")      //all files
+////        intent.setType("text/xml")   //XML file only
+//        intent.addCategory(Intent.CATEGORY_OPENABLE)
+//
+//        try {
+//            ActivityCompat.startActivityForResult(Intent.createChooser(intent, "Select a File to Upload"), FILE_SELECT_CODE);
+//        } catch (android.content.ActivityNotFoundException ex) {
+//            // Potentially direct the user to the Market with a Dialog
+//            Toast.makeText(this, "Please install a File Manager.", Toast.LENGTH_SHORT).show();
+//
+//        }
+//    }
 
 }
